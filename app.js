76f7215769beb15537279eb3ca195db1ab681256
nardicastro <nardi.castro@gmail.com>
2015-07-25 14:02:15 +0200

    Creación de preguntas

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(partials());
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,7 +26,7 @@ app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    var err = new Error('Not Encontrado');
     err.status = 404;
     next(err);
 });
--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,11 @@ router.get('/', function(req, res) {
 // Autoload de comandos con :quizId
 router.param('quizId',quizController.load);  // Autoload :quizId
 
-// Definición de rutas de /quizes 
+// Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',                  quizController.new);
+router.post('/quizes/create',               quizController.create);
 
 module.exports = router;
