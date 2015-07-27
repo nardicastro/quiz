cbc9027ecf5e63ac049349733d7d640d3fe3719a
nardicastro <nardi.castro@gmail.com>
2015-07-27 19:30:41 +0200

    Índice temático

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -53,7 +53,7 @@ exports.answer = function(req, res)
 //GET /quizes /new
 exports.new = function(rep,res){
   var quiz = models.Quiz.build( // crea objeto quiz
-  {pregunta: "Pregunta", respuesta: "Respuesta"}
+  {pregunta: "Pregunta", respuesta: "Respuesta", tema: "Tema"}
 );
 
 res.render('quizes/new',{quiz: quiz, errors: []});
@@ -69,7 +69,7 @@ exports.create = function(req,res){
     }
     else {
       quiz // save: guarda eb BBDD campos pregunta y respuesta de quiz
-      .save({fields: ["pregunta", "respuesta"]})
+      .save({fields: ["pregunta", "respuesta", "tema"]})
       .then(function(){ res.redirect('/quizes');});
       // Redirección HTTP (URL relativo) lista de preguntas
       }
@@ -88,6 +88,7 @@ exports.edit = function(req,res){
 exports.update = function(req,res){
   req.quiz.pregunta = req.body.quiz.pregunta;
   req.quiz.respuesta = req.body.quiz.respuesta;
+  req.quiz.tema = req.body.quiz.tema;
 
   req.quiz.
   validate().
@@ -97,7 +98,7 @@ exports.update = function(req,res){
     }
     else {
       req.quiz // save: guarda eb BBDD campos pregunta y respuesta de quiz
-      .save({fields: ["pregunta", "respuesta"]})
+      .save({fields: ["pregunta", "respuesta", "tema"]})
       .then(function(){ res.redirect('/quizes');});
       // Redirección HTTP (URL relativo) lista de preguntas
       }
